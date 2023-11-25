321e521d6318b8b4b9160ff040bc0845bb34452a
Frank Corso <frank@frankcorso.me>
2023-11-25 13:09:25 -0500

    Bump minimum PHP version

--- litesurveys-wordpress-plugin.php
+++ litesurveys-wordpress-plugin.php
@@ -4,7 +4,7 @@
  * Description: Adds your LiteSurveys to your WordPress site
  * Version: 1.0.0
  * Requires at least: 5.8
- * Requires PHP: 7.4
+ * Requires PHP: 8.0
  * License: GPLv3
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html
  * Author: LiteSurveys
