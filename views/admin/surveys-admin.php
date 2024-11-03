13f5cecfbda05bbb6f560c4138ddb84e90dbcd06
Frank Corso <frank@frankcorso.me>
2024-11-03 17:31:27 -0500

    Remove bulk actions for now and add internationalization to main page

--- views/admin/surveys-admin.php
+++ views/admin/surveys-admin.php
@@ -1,68 +1,71 @@
 <?php
-// Exits if accessed directly.
-defined('ABSPATH') or die('Direct access not permitted.');
+defined('ABSPATH') || exit;
+
+if (!current_user_can('manage_options')) {
+	wp_die(__('You do not have sufficient permissions to access this page.', 'litesurveys'));
+}
 ?>
-<div class="wrap">
+<<div class="wrap">
 	<h1 class="wp-heading-inline">LiteSurveys</h1>
-	<a href="<?php echo admin_url('admin.php?page=litesurveys&action=new'); ?>" class="page-title-action">Add New Survey</a>
+	<a href="<?php echo esc_url(admin_url('admin.php?page=litesurveys&action=new')); ?>" class="page-title-action">
+		<?php _e('Add New Survey', 'litesurveys'); ?>
+	</a>
 	
 	<hr class="wp-header-end">
-
-	<div class="tablenav top">
-		<div class="alignleft actions">
-			<select name="bulk-action">
-				<option value="-1">Bulk Actions</option>
-				<option value="delete">Delete</option>
-				<option value="activate">Activate</option>
-				<option value="deactivate">Deactivate</option>
-			</select>
-			<input type="submit" class="button action" value="Apply">
-		</div>
-		<div class="tablenav-pages">
-			<!-- Pagination would go here -->
-		</div>
-	</div>
 
 	<table class="wp-list-table widefat fixed striped">
 		<thead>
 			<tr>
-				<td class="manage-column column-cb check-column">
-					<input type="checkbox">
-				</td>
-				<th scope="col" class="manage-column">Survey Name</th>
-				<th scope="col" class="manage-column">Status</th>
-				<th scope="col" class="manage-column">Responses</th>
-				<th scope="col" class="manage-column">Created</th>
+				<th scope="col" class="manage-column"><?php _e('Survey Name', 'litesurveys'); ?></th>
+				<th scope="col" class="manage-column"><?php _e('Status', 'litesurveys'); ?></th>
+				<th scope="col" class="manage-column"><?php _e('Responses', 'litesurveys'); ?></th>
+				<th scope="col" class="manage-column"><?php _e('Created', 'litesurveys'); ?></th>
 			</tr>
 		</thead>
 		<tbody>
 			<?php if (empty($surveys)) : ?>
 				<tr>
-					<td colspan="5">No surveys found. <a href="<?php echo admin_url('admin.php?page=litesurveys&action=new'); ?>">Create your first survey</a>.</td>
+					<td colspan="4">
+						<?php _e('No surveys found.', 'litesurveys'); ?> 
+						<a href="<?php echo esc_url(admin_url('admin.php?page=litesurveys&action=new')); ?>">
+							<?php _e('Create your first survey', 'litesurveys'); ?>
+						</a>.
+					</td>
 				</tr>
 			<?php else : ?>
 				<?php foreach ($surveys as $survey) : ?>
 					<tr>
-						<th scope="row" class="check-column">
-							<input type="checkbox" name="surveys[]" value="<?php echo esc_attr($survey->id); ?>">
-						</th>
 						<td>
 							<strong>
-								<a href="<?php echo admin_url('admin.php?page=litesurveys&action=edit&id=' . $survey->id); ?>">
+								<a href="<?php echo esc_url(admin_url('admin.php?page=litesurveys&action=edit&id=' . $survey->id)); ?>">
 									<?php echo esc_html($survey->name); ?>
 								</a>
 							</strong>
 							<div class="row-actions">
 								<span class="edit">
-									<a href="<?php echo admin_url('admin.php?page=litesurveys&action=edit&id=' . $survey->id); ?>">Edit</a> |
+									<a href="<?php echo esc_url(admin_url('admin.php?page=litesurveys&action=edit&id=' . $survey->id)); ?>">
+										<?php _e('Edit', 'litesurveys'); ?>
+									</a> | 
+								</span>
+								<span class="view">
+									<a href="<?php echo esc_url(admin_url('admin.php?page=litesurveys&action=view-responses&id=' . $survey->id)); ?>">
+										<?php _e('View Responses', 'litesurveys'); ?>
+									</a> | 
 								</span>
 								<span class="trash">
-									<a href="<?php echo wp_nonce_url(admin_url('admin.php?page=litesurveys&action=delete&id=' . $survey->id), 'delete-survey_' . $survey->id); ?>" class="submitdelete">Delete</a>
+									<a href="<?php echo wp_nonce_url(admin_url('admin.php?page=litesurveys&action=delete&id=' . $survey->id), 'delete-survey_' . $survey->id); ?>" 
+									   class="submitdelete">
+										<?php _e('Delete', 'litesurveys'); ?>
+									</a>
 								</span>
 							</div>
 						</td>
 						<td>
-							<?php echo $survey->active ? '<span class="status-active">Active</span>' : '<span class="status-inactive">Inactive</span>'; ?>
+							<?php if ($survey->active) : ?>
+								<span class="status-active"><?php _e('Active', 'litesurveys'); ?></span>
+							<?php else : ?>
+								<span class="status-inactive"><?php _e('Draft', 'litesurveys'); ?></span>
+							<?php endif; ?>
 						</td>
 						<td>
 							<?php
@@ -74,7 +77,7 @@ defined('ABSPATH') or die('Direct access not permitted.');
 							?>
 						</td>
 						<td>
-							<?php echo esc_html(mysql2date('F j, Y', $survey->created_at)); ?>
+							<?php echo esc_html(mysql2date(get_option('date_format'), $survey->created_at)); ?>
 						</td>
 					</tr>
 				<?php endforeach; ?>
