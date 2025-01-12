d9512677513bb12c2f25a049e66ebf87351fc5c9
Frank Corso <frank@frankcorso.me>
2025-01-12 14:47:37 -0500

    Fix inconsistent minimum WP version and bump it to 6.2

--- litesurveys-wordpress-plugin.php
+++ litesurveys-wordpress-plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: LiteSurveys
  * Description: Adds simple one-question surveys to your WordPress site
  * Version: 2.0.1
- * Requires at least: 6.1
+ * Requires at least: 6.2
  * Requires PHP: 8.0
  * License: GPLv3
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html
--- readme.txt
+++ readme.txt
@@ -1,7 +1,7 @@
 === LiteSurveys ===
 Contributors: fpcorso
 Tags: surveys, polls, sales, marketing
-Requires at least: 6.0
+Requires at least: 6.2
 Tested up to: 6.7.1
 Stable tag: 2.0.1
 Requires PHP: 8.0
